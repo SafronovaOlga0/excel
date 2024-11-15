5d2bf7c5b4dad8e6330b44e2d773f346ba125082
SafronovaOlga0 <safolga2006@yandex.com>
2024-11-15 14:58:33 +0300

    first commit

---
+++ examples/faker.py
@@ -0,0 +1,6 @@
+from faker import Faker
+from random import randrange
+
+def data_samples():
+    fk = Faker('ru_RU')
+    return [fk.name(), fk.phone_number(), randrange(0,100), randrange(0,100) for _ in range(20)]
